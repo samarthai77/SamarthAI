7571efdd20f6bd0ea30ac2269626ab35236af4a8
samarthai77 <shekharsingh12893@gmail.com>
2026-09-05 21:41:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-
+app.use(express.static('frontend'));
+app.use(express.static(__dirname));
 // ======= Routes =========
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
