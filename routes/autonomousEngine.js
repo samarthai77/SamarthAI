4ab612ad08dd399497f7a7b6bc7c42fd275f8375
samarthai77 <shekharsingh12893@gmail.com>
2026-09-10 13:32:22 +0530

    Update autonomousEngine.js

--- routes/autonomousEngine.js
+++ routes/autonomousEngine.js
@@ -6,11 +6,11 @@ const gpsRoutes = require("./gps");
 const sosRoutes = require("./sos");
 const scannerRoutes = require("./scanner");
 const familyRoutes = require("./family");
-
+const memoryRoutes = require("./memory");
 router.use("/chat", chatRoutes);
 router.use("/gps", gpsRoutes);
 router.use("/sos", sosRoutes);
 router.use("/scanner", scannerRoutes);
 router.use("/family", familyRoutes);
-
+router.use("/memory", memoryRoutes);
 module.exports = router;
