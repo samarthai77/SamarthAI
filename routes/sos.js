0dd37ea6c46e0d7e61e51daff2eb6c359a885776
samarthai77 <shekharsingh12893@gmail.com>
2026-09-11 19:27:04 +0530

    Update sos.js

--- routes/sos.js
+++ routes/sos.js
@@ -76,7 +76,7 @@ if (alerts.length > 0) {
         console.error('ALERT INSERT ERROR:', alertError);
     }
 }       
-    }
+    
     res.status(201).json({
       message: 'SOS alert created successfully',
       sos
