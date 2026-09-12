94da8344d97590462aeb136ba94a9ca14766976e
samarthai77 <shekharsingh12893@gmail.com>
2026-09-12 18:57:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`SamarthAI server running on port ${PORT}`);
-});
+});    
+
