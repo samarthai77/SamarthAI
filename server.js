2f9a0d613044062f89ae35beb6d0ae3c279422ef
samarthai77 <shekharsingh12893@gmail.com>
2026-09-12 17:20:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+app.use(express.json({ limit: '10mb' }));
 app.get('/css/style.css', (req, res) => {
   res.sendFile(__dirname + '/frontend/style.css');
 });
